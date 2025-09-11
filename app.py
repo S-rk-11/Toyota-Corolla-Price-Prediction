6844469278f589b2ebde00c464055b62b2b9022d
Shivani Kalghatgi <shivanikalghatgi@gmail.com>
2025-09-11 22:51:06 +0530

    app.py

--- app.py
+++ app.py
@@ -2,13 +2,50 @@ import streamlit as st
 import pandas as pd
 import pickle
 
+# Page Config
+st.set_page_config(
+    page_title="Toyota Corolla Price Prediction",
+    page_icon="🚗",
+    layout="centered"
+)
+
+#Background Image
+def set_bg_image(image_path):
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("{image_path}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }}
+        .stApp::before {{
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(255, 255, 255, 0.3); /* overlay */
+            z-index: -1;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+# Set your image path here
+set_bg_image("image/Toyota_bg.jpg")
+
 # Load saved model
 with open("models/toyota_random_forest_pipeline.pkl", "rb") as f:
     model = pickle.load(f)
 
 st.title("Toyota Corolla Price Prediction 🚗")
 
-# Example inputs
+# User inputs
 year = st.number_input("Year of Manufacturing", 1990, 2025, 2010)
 km = st.number_input("Kilometers Driven", 0, 500000, 50000)
 hp = st.number_input("Horsepower (HP)", 50, 300, 100)
