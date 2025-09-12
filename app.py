3659a6564c5903ab183834b80828ae1cacd59e69
Shivani Kalghatgi <shivanikalghatgi@gmail.com>
2025-09-12 15:18:18 +0530

    app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def set_bg_image(image_path):
 set_bg_image("image/Toyota_bg.jpg")
 
 # Load saved model
-with open ("models/random_forest.pkl", "rb") as f:
+with open ("models/random_forest_model.pkl", "rb") as f:
   model = pickle.load(f)
 
 with open ("models/scaler_model.pkl", "rb") as f:
